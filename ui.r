a1dd8d401cfeb79e13e9b42422e9ae39825fb56e
shaokangyang86 <60745587+shaokangyang86@users.noreply.github.com>
2021-04-14 04:42:08 +0200

    Add files via upload

---
+++ ui.r
@@ -0,0 +1,24 @@
+library(shiny)
+library(DT)
+showtext_auto()
+fluidPage(
+  headerPanel('IMDB TOP250'),
+  fluidPage(
+    fluidRow(
+      column(3,
+             sliderInput("rate", "rate",
+                         min = 8.3, max = 9.7, value = c(8.3,9.7)),
+      )
+    ),
+  ),
+  fluidRow(
+    column(10,
+           DT::dataTableOutput('table')
+    ),
+  ),
+  fluidRow(
+    column(6,
+           plotOutput(outputId = "plot1")
+    )
+  ),
+)
